7809dacfc6dd0f71d07823b1950e47be65e67faa
Jack Atkinson <jwa34@cam.ac.uk>
2026-07-02 10:51:20 +0100

    Map r_r to real64 in precision module instead of selected_real_kind.

--- src/mod_precision.f90
+++ src/mod_precision.f90
@@ -2,5 +2,5 @@
 module precision_module
   use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
   implicit none
-  integer, parameter :: r_2 = SELECTED_REAL_KIND(12, 60) !! default real kind (12-digit precision, exponent range 60)
+  integer, parameter :: r_2 = dp
 end module precision_module
