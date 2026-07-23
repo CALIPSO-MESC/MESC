2010850e7a47b577a2ea9c5b1649cd53dc249983
Joe Wallwork <jw2423@cam.ac.uk>
2026-07-23 10:07:15 +0100

    Add missing implicit none

--- src/mod_precision.f90
+++ src/mod_precision.f90
@@ -1,4 +1,5 @@
 !> Fortran 2008 kind parameters used throughout MESC
 module precision_module
   use, intrinsic :: iso_fortran_env, only: sp => real32, dp => real64
+  implicit none
 end module precision_module
